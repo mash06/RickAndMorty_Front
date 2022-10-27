0d64fc28005e0e1045dfbd43fb68420b1e09785a
mash06 <mashulia200106@mail.ru>
2022-10-27 23:34:54 +0400

    Maria

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpErrorResponse} from '@angular/common/http';
+import{ModalDismissReasons,NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {EmployeeService} from '../home/employee.service';
+
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  closeResult:String;
+  constructor(
+    private modalService:NgbModal){}
+  ngOnInit(): void {
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+
+  }
+
+}
